e640f05891b4f8698aee96aa6d4c583a197ede12
alipurtas <deveci.tuba13@gmail.com>
2025-07-05 16:10:18 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "a cute girl in tokio",
     };
 
     const response = await env.AI.run(
